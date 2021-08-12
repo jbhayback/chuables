152b37eb5c042e406a62b0fd1cd810e2ad5fbe24
jbhayback <jbhayback@gmail.com>
2021-08-12 23:50:57 +0800

    Initial commit.

---
+++ chuables/chuables/urls.py
@@ -0,0 +1,51 @@
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls import include, url
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+from django.conf.urls.static import static
+from dashboard.views import ProcessLoginView, DashBoardView, UserLogoutView, UserSignUpView
+
+from auth.views import AuthApiListView
+from utils.router import DefaultRouterWithAPIViews
+
+admin.site.site_header = settings.ADMIN_SITE_HEADER
+
+router = DefaultRouterWithAPIViews()
+router.register('chuables/auth', AuthApiListView)
+
+schema_view = get_schema_view(title=settings.API_BROWSER_HEADER, public=True)
+doc_urls = include_docs_urls(title=settings.API_BROWSER_HEADER)
+api_browser_urls = include('rest_framework.urls')
+auth_urls = include('auth.urls')
+users_urls = include('users.urls')
+
+urlpatterns = [
+    path('chuables/', auth_urls),
+    path('chuables/users/', users_urls),
+    path('chuables/docs/', doc_urls),
+    path('chuables/schema/', schema_view),
+    path('chuables/browser/', api_browser_urls),
+    path('chuables/admin/', admin.site.urls),
+    path('', DashBoardView.as_view()),
+    url(r'^login/?$', ProcessLoginView.as_view()),
+    url(r'^dashboard/?$', DashBoardView.as_view()),
+    url(r'^logout/?$', UserLogoutView.as_view()),
+    url(r'^signup/?$', UserSignUpView.as_view()),
+]
+
+urlpatterns += router.urls
+
+# Serve media assets for development, only works while DEBUG=True
+# https://docs.djangoproject.com/en/2.0/howto/static-files/#serving-files-uploaded-by-a-user-during-development
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ chuables/dashboard/views.py
@@ -0,0 +1,105 @@
+import requests
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+from django.shortcuts import redirect, render
+from django.utils.translation import ugettext_lazy as _
+from django.views import View
+
+from rest_framework import status
+
+User = get_user_model()
+
+# Helper functions
+def construct_message(response_data):
+    messages = ""
+    for k,v in response_data.json().items():
+        messages += f" {k}: {v}"
+    return messages
+
+def check_session_if_exists(request):
+    return request.session.get("userid") != None
+
+
+class ProcessLoginView(View):
+    def get(self, request):
+        if check_session_if_exists(request):
+            return redirect('/dashboard')
+        else:
+            return render(request, 'login.html')
+
+    def post(self, request):
+        url = 'http://localhost:8000/chuables/login'
+        payload = {"username":request.POST.get("username"),"password": request.POST.get("password")}
+        response_data = requests.post(url, data=payload)
+        print(response_data.json())
+
+        if response_data.status_code == status.HTTP_200_OK:
+            data = response_data.json()
+            request.session['token'] = data.get("token")
+            request.session['username'] = data.get("username")
+            request.session['userid'] = data.get("userid")
+            return redirect('/dashboard')
+        else:
+            message = {}
+            message['error_message'] = construct_message(response_data)
+            return render(request, "login.html", message)
+
+class DashBoardView(View):
+    def get(self, request):
+        if check_session_if_exists(request):
+            permissions = self._get_user_roles_permissions(request)
+            context = {}
+            context['permissions'] = permissions
+            return render(request, 'dashboard.html', context)
+        else:
+            return redirect('/login')
+
+    def _get_user_roles_permissions(self, request):
+        userid = request.session.get("userid")
+        url = f"http://localhost:8000/chuables/users/{userid}/roles"
+        headers={'Content-Type':'application/x-www-form-urlencoded', 'Authorization':f"Token {request.session.get('token').get('auth_token')}"}
+        response_data = requests.get(url, headers=headers)
+        if response_data.status_code == status.HTTP_200_OK:
+            data = response_data.json()
+            return self._get_user_permissions(data)
+        return []
+
+    @staticmethod
+    def _get_user_permissions(data):
+        permissions = [Permission.objects.filter(group__id=id) for id in data.values()]
+        if permissions:
+            return [list(permission.values())[0] for permission in permissions]
+
+        return []
+
+class UserLogoutView(View):
+    def get(self, request):
+        return redirect('/login')
+
+    def post(self, request):
+        if check_session_if_exists(request):
+            del request.session['token']
+            del request.session['username']
+            del request.session['userid']
+
+        return redirect('/login')
+
+class UserSignUpView(View):
+    def get(self, request):
+        if check_session_if_exists(request):
+            return redirect('/dashboard')
+        return render(request, 'signup.html')
+
+    def post(self, request):
+        url = 'http://localhost:8000/chuables/signup'
+        payload = {"username":request.POST.get("username"),
+            "email": request.POST.get("email"),
+            "password": request.POST.get("password")}
+        response_data = requests.post(url, data=payload)
+        message = {}
+        if response_data.status_code == status.HTTP_201_CREATED:
+            message['success_message'] = "Registration Successful! You may now login."
+        else:
+            message['error_message'] = construct_message(response_data)
+        return render(request, 'signup.html', message)
